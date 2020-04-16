37b8cd4ffae71eaab383dcfb72bcba209d64d977
Mierenga <mike.swierenga@gmail.com>
2020-04-15 20:40:38 -0400

    Fix logic and ensure notification only shows once per new commit.

--- git-informed.sh
+++ git-informed.sh
@@ -1,14 +1,17 @@
+prev_remote_ahead=0
 while :
 do
   git fetch origin
   branch=`git branch --show-current`
   compare=`git rev-list --left-right --count origin/${branch}...${branch}`
   compare_array=($compare)
-  echo remote: ${compare_array[0]} ahead
+  remote_ahead=${compare_array[1]}
+  echo remote: ${remote_ahead} ahead
   echo local:  ${compare_array[1]} ahead
-  message="(${branch})"
-  if (( ${compare_array[1]} > 0 )); then
+  message="(${branch}) is ahead by "
+  if (( ${remote_ahead} > ${prev_remote_ahead} )); then
     eval "osascript -e 'display notification \"$message\" with title \"⌛️\"'" 
   fi
+  prev_remote_ahead=${remote_ahead}
   sleep 10
 done
